3b5543bcfe36f55f74ebb7fbf1eea3baa87a3c71
Nweke Onyekachi <nocrah4@gmail.com>
2022-11-04 17:25:38 +0100

    recent changes

--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,35 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template, request, flash
 
 auth=Blueprint('auth', __name__)
 
-@auth.route('/register')
-def index():
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data=request.form
+    print(data)
+    return render_template("login.html")
+
+@auth.route('/register',  methods=['GET', 'POST'])
+def register():
+    if request.method=='POST':
+        email=request.form.get('email')
+        firstName=request.form.get('name')
+        password=request.form.get('password')
+        password2=request.form.get('password2')
+        
+        if len(email)<4:
+            flash('email must be more than 4 characters', category='error')
+            
+        elif len(firstName)<2:
+            flash('first name must be more than 6 characters', category='error')
+        elif password != password2:
+            flash('password must be same more than 4 characters', category='error')
+        elif len(password)<7:
+            flash('password must be more than 7 characters', category='error')
+        else:
+            #add user to database
+            flash('Account created successfully', category='success')
+            
+    
     return render_template("register.html")
 
 
